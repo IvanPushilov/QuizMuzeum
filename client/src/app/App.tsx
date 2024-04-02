cf2a48ad534e20baf79b7d780d361ace25d8f69c
Ivan Pushilov <pusilovivan@gmail.com>
2024-04-02 17:58:40 +0500

    цукацуа

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,15 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router';
 import Main from '../features/Main/components/Main';
 import RegistrationPage from '../features/Auth/components/RegistrationPage';
+import AuthorisationPage from '../features/Auth/components/AuthorisationPage';
 
 function App(): JSX.Element {
-
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(authCheckUser()).catch(console.log)
-  }, [])
 
   return (
 <Routes>
