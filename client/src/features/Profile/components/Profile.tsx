feb9ea24918a10cf455356a43767e3037c1afa46
Dianaanaas <nasibullina.di@yandex.ru>
2024-04-08 16:43:30 +0300

    fix:disabled style

--- client/src/features/Profile/components/Profile.tsx
+++ client/src/features/Profile/components/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import '../styles/reglog.css';
 import type { RootState} from '../../../store/store';
 import { useAppDispatch } from '../../../store/store';
 
