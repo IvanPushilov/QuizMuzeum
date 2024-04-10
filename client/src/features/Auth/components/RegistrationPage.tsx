4e87d88e081373d84d710271a9c9514ef7f4a8fc
Mark Kim <mark@Mark-MacBook-Pro.local>
2024-04-11 01:25:44 +0500

    feat: added classNames

--- client/src/features/Auth/components/RegistrationPage.tsx
+++ client/src/features/Auth/components/RegistrationPage.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { yupResolver } from '@hookform/resolvers/yup';
-import React  from 'react';
+import React, { useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
@@ -29,6 +29,8 @@ function RegistrationPage(): JSX.Element {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  const [checked, setChecked] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -41,61 +43,70 @@ function RegistrationPage(): JSX.Element {
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-offset-3 col-md-6">
-          <form className="form-horizontal" onSubmit={handleSubmit(registration)}>
-            <span className="heading">Регистрация</span>
+    <div className="auth-form">
+  <div className="auth-form__container">
+    <div className="auth-form__content">
+      <form className="auth-form__form" onSubmit={handleSubmit(registration)}>
+      
 
-              
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Ваше имя"
-                    {...register('name')}
-                  />
-                  <i className="fa fa-user" />
-                  <div className="color-er">{errors.name?.message}</div>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Электронная почта"
-                    {...register('email')}
-                  />
-                  <i className="fa fa-user" />
-                  <div className="color-er">{errors.email?.message}</div>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Пароль"
-                    {...register('password')}
-                  />
-                  <i className="fa fa-user" />
-                  <div className="color-er">{errors.password?.message}</div>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Повторите пароль"
-                    {...register('rpassword')}
-                  />
-                  <i className="fa fa-user" />
-                  <div className="color-er">{errors.rpassword?.message}</div>
-                </div>
-                <div className="form-group">
-                  <button type="submit" className="btn btn-default">
-                    ВХОД
-                  </button>
-                </div>
-                </form>
-              </div>
-              </div>
-              </div>
-  )}
+        <div className="auth-form__input-group">
+          <input
+            type="text"
+            className="auth-form__input"
+            placeholder="Ваше имя"
+            {...register('name')}
+          />
+          <i className="auth-form__icon" />
+          <div className="auth-form__error">{errors.name?.message}</div>
+        </div>
+        <div className="auth-form__input-group">
+          <input
+            type="email"
+            className="auth-form__input"
+            placeholder="Электронная почта"
+            {...register('email')}
+          />
+          <i className="auth-form__icon" />
+          <div className="auth-form__error">{errors.email?.message}</div>
+        </div>
+        <div className="auth-form__input-group">
+          <input
+            type="password"
+            className="auth-form__input"
+            placeholder="Пароль"
+            {...register('password')}
+          />
+          <i className="auth-form__icon" />
+          <div className="auth-form__error">{errors.password?.message}</div>
+        </div>
+        <div className="auth-form__input-group">
+          <input
+            type="password"
+            className="auth-form__input"
+            placeholder="Повторите пароль"
+            {...register('rpassword')}
+          />
+          <i className="auth-form__icon" />
+          <div className="auth-form__error">{errors.rpassword?.message}</div>
+        </div>
+        <label>
+          <input
+            className="auth-form__checkbox"
+            onChange={(e) => setChecked(e.target.checked)}
+            checked={checked}
+            type="checkbox"
+          />
+          Согласен с условиями обработки персональных данных
+        </label>
+        <div className="auth-form__form-group">
+          <button disabled={!checked} type="submit" className="auth-form__button">
+            Регистрация
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+  );
+}
 export default RegistrationPage;
