7c2ab4c3b910f168a4d08514bd922b12c349437e
Mark Kim <mark@Mark-MacBook-Pro.local>
2024-04-12 02:42:56 +0500

    feat: pagination

--- client/src/features/Posts/components/PostsPage.tsx
+++ client/src/features/Posts/components/PostsPage.tsx
@@ -3,6 +3,10 @@ import PostItem from './PostItem';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store/store';
 import FormAddPost from '../../Posts/components/FormAddPost';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import '../styles/post.css';
 
 
@@ -15,9 +19,36 @@ function PostsPage(): JSX.Element {
        <div>
         {user?.role === 'admin' && <p>Добавить пост</p> && <FormAddPost />}
       </div>
-      {posts.map((post) => (
-        <PostItem key={post.id} post={post} />
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+              {posts.map((post) => (
+                <div className='post__item' >
+       <SwiperSlide> <PostItem key={post.id} post={post} /></SwiperSlide>
+       </div>
       ))}
+      </Swiper>
+
     </div>
   );
 }
