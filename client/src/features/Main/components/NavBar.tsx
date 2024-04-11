2f9f2676126ede8742da5960cd8814e07bb34f1b
Ivan Pushilov <pusilovivan@gmail.com>
2024-04-11 16:34:24 +0500

    add_home_btn

--- client/src/features/Main/components/NavBar.tsx
+++ client/src/features/Main/components/NavBar.tsx
@@ -24,12 +24,12 @@ function NavBar(): JSX.Element {
     <nav className="page__menu page__custom-settings menu">
       <ul className="menu__list r-list">
       <li className="menu__group">
-        <NavLink className='link-vk' to='https://vk.com/synergyoil86'>
-      <svg width="40px" height="40px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path class="a" d="M27.55,35.19V28.55c4.46.68,5.87,4.19,8.71,6.64H43.5a29.36,29.36,0,0,0-7.9-10.47c2.6-3.58,5.36-6.95,6.71-12.06H35.73c-2.58,3.91-3.94,8.49-8.18,11.51V12.66H18l2.28,2.82,0,10.05c-3.7-.43-6.2-7.2-8.91-12.87H4.5C7,20.32,12.26,37.13,27.55,35.19Z"/></svg>
+        <NavLink className='link-vk' to='https://vk.com/synergyoil86'target='_blank'>
+      <svg width="40px" height="40px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path  d="M27.55,35.19V28.55c4.46.68,5.87,4.19,8.71,6.64H43.5a29.36,29.36,0,0,0-7.9-10.47c2.6-3.58,5.36-6.95,6.71-12.06H35.73c-2.58,3.91-3.94,8.49-8.18,11.51V12.66H18l2.28,2.82,0,10.05c-3.7-.43-6.2-7.2-8.91-12.87H4.5C7,20.32,12.26,37.13,27.55,35.19Z"/></svg>
       </NavLink>
       </li>
       <li>
-
+<NavLink className='link-home' to='/' ><img width="40px" height="40px" src='../../../../public/profileImg/home.png'/></NavLink>
       </li>
 
 
