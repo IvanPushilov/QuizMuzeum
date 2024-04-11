125869907a9830a728258d76a56c117f44f6f653
Ivan Pushilov <pusilovivan@gmail.com>
2024-04-11 18:04:23 +0500

    t3

--- client/src/features/Main/components/Footer.tsx
+++ client/src/features/Main/components/Footer.tsx
@@ -6,17 +6,17 @@ function Footer(): JSX.Element {
       <div>
         <p>Организаторы:</p>
         <div className='container_org'>
-    <img className='img1' src="../../../public/profileimg/dep.png" alt="" />
-    <img className='img2' src="../../../public/profileimg/efcf6923f27a2593c68c15a714da2e9a.png" alt="" />
-    <img className='img3' src="../../../public/profileimg/123.png" alt="" />
+    <img className='img1' src="../../../../public/profileImg/dep.png" alt="" />
+    <img className='img2' src="../../../../public/profileImg/efcf6923f27a2593c68c15a714da2e9a.png" alt="" />
+    <img className='img3' src="../../../../public/profileImg/123.png" alt="" />
     </div>
       </div>
       <div>
         <p>Партнеры:</p>
         <div className='container_part'>
-      <img className='img4' src="../../../public/profileimg/logo 1.png" alt="" />
-      <img className='img5' src="../../../public/profileimg/LGO-СНГПР-с-надписью-07-12-18 1.png" alt="" />
-      <img className='img6' src="../../../public/profileimg/рго 1.png" alt="" />
+      <img className='img4' src="../../../../public/profileImg/logo 1.png" alt="" />
+      <img className='img5' src="../../../../public/profileImg/LGO-СНГПР-с-надписью-07-12-18 1.png" alt="" />
+      <img className='img6' src="../../../../public/profileImg/рго 1.png" alt="" />
       </div>
       </div>
     </div>
